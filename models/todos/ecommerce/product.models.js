d585819ac1ff901743edf9db29f652f3f5b373e8
Pratik Das <daspratik0810@gmail.com>
2026-07-17 18:59:14 +0000

    ecommerce model completed

--- models/todos/ecommerce/product.models.js
+++ models/todos/ecommerce/product.models.js
@@ -28,7 +28,7 @@ const productSchema = new mongoose.Schema(
             ref : "Category",
             required : true 
         },
-        user : {
+        owner : {
             type : mongoose.Schema.Types.ObjectId,
             ref : "User" 
         },
